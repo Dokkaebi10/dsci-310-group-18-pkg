b22b7c19b2fb72d358095a3157429e68e9f42027
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-04-09 12:56:48 -0700

    removed unneeded tests

--- tests/tests.R
+++ tests/tests.R
@@ -20,26 +20,6 @@ test_that("The number of variables is not the same!", {
 test_that("The results are not the same!", {
           expect_equal(min(plot$accuracy), 0.426789675597178, tolerance = 0.05)})
 
-source("../R/place_data.R")
-# test that there are the right number of columns
-test_that("The number of columns for Switzerland is not the same!", {
-    expect_equal(ncol(web_data('switzerland', url_switzerland)), 13)})
-test_that("The number of columns for cleveland is not the same!", {
-    expect_equal(ncol(web_data('cleveland', url_cleveland)), 13)})
-
-# test that there are 200 rows
-test_that("The number of rows in va are not the same!", {
-    expect_equal(nrow(web_data('va', url_va)), 200)})
-test_that("The number of rows in hungary are not the same!", {
-    expect_equal(nrow(web_data('hungary', url_hungarian)), 200)})
-
-source("../R/joining_data.R")
-# testing the function clean_csv produces dataframes with correct data types
-test_that("thal_f is not a factor (it should be)", {
-    expect_true(is.factor(clean_csv('../data/raw/cleveland.csv')$thal_f))})
-test_that("age is not numeric", {
-    expect_true(is.numeric(clean_csv('../data/raw/va.csv')$age))})
-
 source("../R/boxplots.R")
 # Setup
 heart_data <- read_csv('../data/processed/heart_data.csv')
