00956219d2865951565eb4171147bbe3f64231d6
leuneri <ericleung147258369@hotmail.com>
2023-04-10 00:32:14 -0800

    one warning down

--- R/classification_model.R
+++ R/classification_model.R
@@ -1,36 +1,37 @@
 library(caret)
 library(tidyverse)
 library(tidymodels)
+library(kknn)
 set.seed(1)
-#' K visualization 
-#' 
+#' K visualization
+#'
 #' @param data a dataset used to be plot accuracy versus K
 #' @param split is the number of diagnosis
 #' @param gridFrom is the gridvals value
 #' @param gridTo is the gridvals value
 #'
-#' 
+#'
 #'@return a line graph with the accuracy estimates versus the number of neighbors
-#' 
-#'@examples 
-#' classifier(heart_training, 5, 1, 21)
+#'
+#'@examples
+#' classifier(heart_training)
 classifier <- function(data, split =5 , gridFrom = 1, gridTo = 21) {
   set.seed(1)
-  
+
   recipe <- recipe(diagnosis_f ~ ., data) %>%
       step_scale(all_predictors()) %>%
       step_center(all_predictors())
-    
+
   heart_data_recipe <<- recipe
-  
+
   spec <- nearest_neighbor(weight_func = "rectangular", neighbors = tune()) %>%
       set_engine("kknn") %>%
       set_mode("classification")
-  
+
   vfold <- vfold_cv(data, v = split, strata = diagnosis_f)
-  
+
   gridvals <- tibble(neighbors = seq(from = gridFrom, to = gridTo))
-  
+
   results <- workflow() %>%
       add_recipe(recipe) %>%
       add_model(spec) %>%
