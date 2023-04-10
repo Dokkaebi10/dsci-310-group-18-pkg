647f5c53d3f9654f39adf681c5a0d7b9523287aa
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-04-10 04:26:46 -0700

    added csv path

--- tests/testthat/test-boxplots.R
+++ tests/testthat/test-boxplots.R
@@ -1,9 +1,8 @@
 library(testthat)
 library(tidyverse)
 
-source("../R/boxplots.R")
 # Setup
-heart_data <- read_csv('../data/processed/heart_data.csv')
+heart_data <- read_csv("https://raw.githubusercontent.com/Dokkaebi10/dsci-310-group-18/main/data/processed/heart_data.csv")
 boxplot_age <- grid_boxplot(heart_data, age, "Age (years)", "A. Boxplot of degree of heart \n disease in relation to patient's \nage")
 boxplot_max_heart_rate <- grid_boxplot(heart_data, max_heart_rate, "Maximum heart rate (BPM)", "C. Boxplot of degree of heart \ndisease in relation to patient's \nmaximum heart rate")
 
