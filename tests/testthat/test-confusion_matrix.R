8b43a0987561284b83f566c14772c7fe013fb394
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-04-10 04:31:02 -0700

    added heart data predict csv

--- tests/testthat/test-confusion_matrix.R
+++ tests/testthat/test-confusion_matrix.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(tidyverse)
 
-heart_data_predict <- read_csv('../data/modelling/predict_data.csv') %>%
+heart_data_predict <- read_csv("https://raw.githubusercontent.com/Dokkaebi10/dsci-310-group-18/main/data/modelling/predict_data.csv") %>%
     mutate(diagnosis_f = as.factor(diagnosis_f), .pred_class = as.factor(.pred_class))
 test_that("Y axis of confusion matrix does not match", {
     expect_equivalent(confusion_matrix(heart_data_predict)$labels$y, "Prediction of diagnosis")})
