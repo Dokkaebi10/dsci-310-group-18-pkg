3cb40e81bbc3f7408fa8fee100324db34cab396f
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-04-10 04:31:44 -0700

    added accuracy data csv

--- tests/testthat/test-model_visualization.R
+++ tests/testthat/test-model_visualization.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(tidyverse)
 
-accuracy_data <- read_csv('../data/modelling/heart_data_accuracies.csv')
+accuracy_data <- read_csv("https://raw.githubusercontent.com/Dokkaebi10/dsci-310-group-18/main/data/modelling/heart_data_accuracies.csv")
 test_that("Y axis is not what we expected", {
     expect_equivalent(knn_visualization(accuracy_data)$labels$y, "Accuracy Estimate")})
 test_that("X axis is not what we expected", {
