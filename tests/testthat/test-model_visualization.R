b3e9c2b062bf5b3f5720bfeb7165b8c54f694b27
Daniel Lee <122393123+Dokkaebi10@users.noreply.github.com>
2023-04-10 03:27:12 -0700

    added test

--- tests/testthat/test-model_visualization.R
+++ tests/testthat/test-model_visualization.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+library(tidyverse)
+
+accuracy_data <- read_csv('../data/modelling/heart_data_accuracies.csv')
+test_that("Y axis is not what we expected", {
+    expect_equivalent(knn_visualization(accuracy_data)$labels$y, "Accuracy Estimate")})
+test_that("X axis is not what we expected", {
+    expect_equivalent(knn_visualization(accuracy_data)$labels$x, "Neighbors")
 })
